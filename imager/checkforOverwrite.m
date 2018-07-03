f37006e823664ba4ecdbfa7f17ce41142fc7f399
Ingie Hong <ingiehong@jhmi.edu>
2018-07-03 15:13:52 -0400

    checkforOverwrite updated to allow for pre-existing folder and proper display.

--- imager/checkforOverwrite.m
+++ imager/checkforOverwrite.m
@@ -1,17 +1,16 @@
 function [flag dd] = checkforOverwrite
 
-global running
-
 animal = get(findobj('Tag','animaltxt'),'String');
 unit   = get(findobj('Tag','unittxt'),'String');
 expt   = get(findobj('Tag','expttxt'),'String');
 datadir= get(findobj('Tag','datatxt'),'String');
 
-dd = [datadir '\' lower(animal) '\u' unit '_' expt]
+dd = [datadir '\' lower(animal) ];
+disp([ 'Folder for data: ' dd ] )
 
 flag = 0;
 
 if(exist(dd))
-    warndlg('Directory exists!!!  Make sure you are not overwritting old data!  Please check current animal, unit and expt values.  I will now abort this sampling request.','!!! Warning !!!')
+    disp('Directory exists!!!  Make sure you are not overwritting old data!  Please check current animal, unit and expt values.  I will now abort this sampling request.')
     flag = 1;
 end
