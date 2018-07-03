4f708ec1b4186633fd077d9350e923202534ad55
Ingie Hong <ingiehong@jhmi.edu>
2018-07-03 15:24:36 -0400

    cleaned up convert_structs

--- analysis/convert_structs.m
+++ analysis/convert_structs.m
@@ -12,12 +12,14 @@ else
 end
 
 %Creating grab file
-tmp=squeeze(im(:,:,1,1));
+tmp=squeeze(mean(im,4));
 grab.img = tmp; %tmp is grab from collect
 grab.clock = [2018 05 31 17 57 14];
 grab.ROIcrop = [];%[40 260 155 250]; 
 grab.comments = {};
-%save('grab_XX0_000_001_31_May_2018_17_57_14.mat', 'grab');
+
+[~,name,~]=fileparts(fName);
+save([pathname name '_grab.mat'], 'grab');
 %dataPath= 'C:\Users\hwanggm1\Documents\data\Retinopathy\ImagerData\05312018\'
  %   load([dataPath '05311800_Widefield_2018-05-31_175714syncInfo.mat']);
 % '05311800_Widefield_2018-05-31_175714syncInfo.mat % azimuth 0
@@ -36,7 +38,7 @@ else
    load(fullfile(pathname, fName), '-mat'); % 
 end
 
-%%
+%% Basically trash...
 %cd('C:\Users\Huganir lab\Documents\imager_data\05312018')
 %xx0_u000_001.analyzer  xx0_u000_002.analyzer are azimuth files 0&180 deg
 %xx0_u000_004.analyzer  xx0_u000_007.analyzer are altitude files 0&180 deg
@@ -51,8 +53,8 @@ end
 % Analyzer.M= setfield(Analyzer.M, 'screenXcm', 96); % update this 
 % Analyzer.M= setfield(Analyzer.M, 'xpixels', 1280);
 % Analyzer.M= setfield(Analyzer.M, 'ypixels', 720);
- Analyzer.M= setfield(Analyzer.M, 'syncSize', 4); %appears to be incorrectly produced
- Analyzer.M= setfield(Analyzer.M, 'running', 1);
+%  Analyzer.M= setfield(Analyzer.M, 'syncSize', 4); %appears to be incorrectly produced
+%  Analyzer.M= setfield(Analyzer.M, 'running', 1);
 % Analyzer.M= setfield(Analyzer.M, 'analyzerRoot', 'C:\Users\hwanggm1\Documents\data\Retinopathy\AnalyzerFiles'); %
  %Analyzer.M= setfield(Analyzer.M, 'stimulusIDP', '10.1.38.61'); %update
  
@@ -60,18 +62,18 @@ end
 %Analyzer.P.param{12}{3}='altitude' %% or 'azimuth'
 
 if Analyzer.P.param{3}{3} ~= 183 %ensure longer stimulus time is used for retonotopic analysis
-  error('your stimulus duration is not 183 seconds')
+  warning('Stimulus duration is not 183 seconds')
 end
-    Analyzer.L.reps=1;
+%     Analyzer.L.reps=1;
 if Analyzer.L.rand==1
-    error('you forgot to switch off random in Looper')
+    warning('Random is on in Looper')
 end
-Analyzer.L.param{1}{2}=0;
-Analyzer.loops.conds{1}.val{1}=0;
-Analyzer.loops.conds{1}.repeats{1}.trialno=1
-Analyzer.loops.conds{2}.symbol{1} = 'ori';
-Analyzer.loops.conds{2}.val{1}=180;
-Analyzer.loops.conds{2}.repeats{1}.trialno=2
+% Analyzer.L.param{1}{2}=0;
+% Analyzer.loops.conds{1}.val{1}=0;
+% Analyzer.loops.conds{1}.repeats{1}.trialno=1
+% Analyzer.loops.conds{2}.symbol{1} = 'ori';
+% Analyzer.loops.conds{2}.val{1}=180;
+% Analyzer.loops.conds{2}.repeats{1}.trialno=2
 
 %cd('C:\Users\hwanggm1\Documents\data\Retinopathy\ImagerData\05312018')
 %load('05311816_Widefield_2018-05-31_181251syncInfo.mat')
@@ -94,7 +96,7 @@ end
 diff(syncInfo.dispSyncs)
 
 syncInfo1=syncInfo ;       
-getStartframe=find(syncInfo1.dispSyncs(1)<syncInfo1.acqSyncs,1)-1
+getStartframe=find(syncInfo1.dispSyncs(1)<syncInfo1.acqSyncs,1) % First frame after photodiode pulse
 syncInfo1.acqSyncs=syncInfo1.acqSyncs(getStartframe:getStartframe+1869);
 %syncInfo1.dispSyncs = [syncInfo1.dispSyncs(1)-2 ; syncInfo1.dispSyncs];
 %%rare fix
@@ -119,8 +121,8 @@ end
        
 diff(syncInfo.dispSyncs)
 syncInfo2=syncInfo ;
-getStartframe=find(syncInfo2.dispSyncs(1)<syncInfo2.acqSyncs,1)-1;
-syncInfo2.dispSyncs=syncInfo2.dispSyncs(1:end); % add missing disp pulse and prune to 1870
+getStartframe=find(syncInfo2.dispSyncs(1)<syncInfo2.acqSyncs,1); % First frame after photodiode pulse
+%syncInfo2.dispSyncs=syncInfo2.dispSyncs(1:end); % add missing disp pulse and prune to 1870
 syncInfo2.acqSyncs=syncInfo2.acqSyncs(getStartframe:getStartframe+1869);
 clear syncInfo
  
