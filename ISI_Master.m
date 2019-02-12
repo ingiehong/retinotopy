b5c821cd0b81ee23a4632e7a8eb7452787adef24
Ingie Hong <ingiehong@jhmi.edu>
2019-02-12 15:38:53 -0500

    Update of startup scripts

--- ISI_Master.m
+++ ISI_Master.m
@@ -1,10 +1,17 @@
+% Script to launch all Master-side apps for visual cortex imaging (imager and Stimulator)
+% 
+% Ingie Hong, Johns Hopkins Medical Institute, 2018
+
+% Please edit configCom %change IP address to match current workstation
+% Acquired data will be saved to C:\Users\USERNAME\Documents\imager_data
+
 %% Close all windows and clear all variables, including hidden globals
 clear all
 close all force % close all windows
 clearvars -global % clear all variables, including hidden globals
-imaqreset % To reset image acquisition toolbox, in case of camera error
+imaqreset % To reset image acquisition toolbox, in case of a camera error
 
-%% Set up the master instance of MATLAB
+%% Set up the PATH and display settings
 addpath(genpath([fileparts(which('ISI_Master')) filesep 'Stimulator_master']));
 configDisplayCom 
 
@@ -23,6 +30,3 @@ movegui(GUIhandles.main.figure1, 'northwest')
 movegui(GUIhandles.looper.figure1, 'northeast')
 movegui(GUIhandles.param.figure1, 'north')
 movegui(imagerhandles.figure1, [500 45])
-
-% %edit configCom %change IP address to match current workstation
-% % data will be saved to C:\Users\Ingie\Documents\imager_data
