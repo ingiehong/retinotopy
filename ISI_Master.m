7e7299631bcb2b496e89c65f733257b6a6126dce
Ingie Hong <ingiehong@jhmi.edu>
2019-02-14 12:27:50 -0500

    Updated imager window position after window enlargement. Removed redundant configDisplayCom call.

--- ISI_Master.m
+++ ISI_Master.m
@@ -13,7 +13,7 @@ imaqreset % To reset image acquisition toolbox, in case of a camera error
 
 %% Set up the PATH and display settings
 addpath(genpath([fileparts(which('ISI_Master')) filesep 'Stimulator_master']));
-configDisplayCom 
+%configDisplayCom 
 
 %% these lines set up the imager from Master MATLAB instance
 addpath([fileparts(which('ISI_Master')) filesep 'imager'])
@@ -29,4 +29,5 @@ global GUIhandles imagerhandles
 movegui(GUIhandles.main.figure1, 'northwest')
 movegui(GUIhandles.looper.figure1, 'northeast')
 movegui(GUIhandles.param.figure1, 'north')
-movegui(imagerhandles.figure1, [500 45])
+ScreenSize=get(0,'ScreenSize');
+movegui(imagerhandles.figure1, [ScreenSize(3)-1280-20 45 ])
