880db00d915345a0d929bcc2240395bf912df733
ingie <ingiehong@jhmi.edu>
2022-09-10 15:04:51 -0400

    Syntax error corrections recommended by Code Compatibility Report.

--- Stimulator_master/DisplayCode/shutterCntrl.m
+++ Stimulator_master/DisplayCode/shutterCntrl.m
@@ -5,7 +5,7 @@ for i = 1:Nparams
     psymbol = looperInfo.conds{cond}.symbol{i};
     if strcmp(psymbol,'Leye_bit')    	
         pval = looperInfo.conds{cond}.val{i};
-    elseif
+    else
         strcmp
     end
 end
