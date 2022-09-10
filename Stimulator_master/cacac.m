f09f28a734bd4a228f83fc42213adf9597f36dd1
Ingie Hong <ingiehong@jhmi.edu>
2022-09-10 00:36:38 -0400

    Added cacac.m for clean Stimulator/imager termination.

---
+++ Stimulator_master/cacac.m
@@ -0,0 +1,6 @@
+% cacac.m : clear all and close all shortcut
+% Ingie Hong, Johns Hopkins Medical Institute, 2017
+close all force % close all windows
+clear all
+clearvars -global % clear all variables, including hidden globals
+imaqreset % To reset image acquisition toolbox, in case of camera error
